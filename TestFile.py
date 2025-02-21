b18ceac7d0f7b139266048846948a3d88aa6cfd3
Kloudshadow <162203639+Kloudshadow@users.noreply.github.com>
2025-02-21 02:47:26 +0000

    first commit

---
+++ TestFile.py
@@ -0,0 +1,20 @@
+"""
+
+Classes to work on: Library (with accompanying book class), Simple Game (if time)
+"""
+
+
+class Book:
+    def __init__(self, title, author, isbn, availability):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.availability = availability
+
+
+
+class Library:
+    def __init__(self):
+        self.book_list = []
+
+
