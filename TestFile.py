a344bdd26108473f30dfdf9d82688319c0f7b203
Kloudshadow <bm.burgess52@gmail.com>
2025-02-24 20:47:45 -0600

    Add methods to describe and remove books in Library
    Updated the `Library` class in `TestFile.py` to include
    `describe_books` for printing book details and `remove_book`
    for removing a book by title. Added a main testing block
    to demonstrate the new functionality with sample books.

--- TestFile.py
+++ TestFile.py
@@ -20,7 +20,36 @@ class Library:
     def add_book(self, book):
         self.book_list.append(book)
 
+    def describe_books(self):
+        for book in self.book_list:
+            print(f"Title: {book.title}, Author: {book.author}, ISBN: {book.isbn}, Availability: {book.availability}")
+
+    def remove_book(self, title):
+        for book in self.book_list:
+            if(book.title == title):
+                self.book_list.remove(book)
+                print(f"Removed {book.title}")
+                break
+
+
+#MAIN
 print("Doing a bit of testing real fast.")
+
+main_library = Library()
+
+book1 = Book("The Lord of the Rings", "Tolkien", 8991148177578, True)
+book2 = Book("I Cheerfully Refuse", "Enger", 4906172209419, False)
+book3 = Book("Black Holes", "Forshaw, Cox", 7672344800463, True)
+
+main_library.add_book(book1)
+main_library.add_book(book2)
+main_library.add_book(book3)
+
+main_library.describe_books()
+
+main_library.remove_book("The Lord of the Rings")
+
+main_library.describe_books()
 
     
 
