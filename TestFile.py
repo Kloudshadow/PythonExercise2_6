97f2971ee03c6f054b9b3eeb86c0d2021ea3fad7
Kloudshadow <bm.burgess52@gmail.com>
2025-02-24 21:21:28 -0600

    Created 'search_book' method, began 'take_loan".
    The 'search_book' method was completed entirely, and the 'take_loan' method was started. Test code was created for the 'search_book' method.

--- TestFile.py
+++ TestFile.py
@@ -16,6 +16,7 @@ class Book:
 class Library:
     def __init__(self):
         self.book_list = []
+        self.loan_list = []
 
     def add_book(self, book):
         self.book_list.append(book)
@@ -30,6 +31,25 @@ class Library:
                 self.book_list.remove(book)
                 print(f"Removed {book.title}")
                 break
+
+
+    #book_present is returned so search_book() can be easily used as the first step of taking out a loan. 
+    def search_book(self, title):
+        book_present = False
+        for book in self.book_list:
+            if(book.title == title):
+                book_present = True
+                print(f"{title} is in the library system. Availability: {book.availability}")
+        if(book_present == False):
+            print(f"{title} is not in the library system.")
+
+        return book_present
+
+    def take_loan(self, title)
+        if(self.search_book(title) == True):
+            self.loan_list.
+
+    #def return_loan(self)
 
 
 #MAIN
@@ -50,6 +70,9 @@ main_library.describe_books()
 main_library.remove_book("The Lord of the Rings")
 
 main_library.describe_books()
+
+main_library.search_book("The Lord of the Rings")
+main_library.search_book("Black Holes")
 
     
 
